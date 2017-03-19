d60cf32af494c85149b2e42160afbaefb0fc329c
Sean Lavine <freewil@users.noreply.github.com>
2017-03-19 14:04:35 -0700

    add tests

---
+++ test/indexTest.js
@@ -0,0 +1,70 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+const async = require('async')
+const dynamodb = require('./util/dynamodb')
+
+describe('DynamoDbWriter', function () {
+  before(function createDynamoDbClient () {
+    this.dynamodb = dynamodb.createClient()
+  })
+
+  beforeEach(function createWriter () {
+    this.writer = dynamodb.createWriter(this.dynamodb)
+  })
+
+  before(function startDynalite (done) {
+    this.dynalite = dynamodb.dynalite(done)
+  })
+
+  before(function createTable (done) {
+    dynamodb.createTable(this.dynamodb, done)
+  })
+
+  after(function deleteTable (done) {
+    dynamodb.deleteTable(this.dynamodb, done)
+  })
+
+  after(function teardownDynamoDb (done) {
+    this.dynalite.close(done)
+  })
+
+  it('should queue up to 24 items without writing', function (done) {
+    async.times(24, (n, next) => {
+      this.writer.save(dynamodb.item(), next)
+    }, (err) => {
+      assert.ifError(err)
+      assert.equal(this.writer.queue.length, 24)
+      done(null)
+    })
+  })
+  it('25th write should trigger flush automatically', function (done) {
+    async.times(25, (n, next) => {
+      this.writer.save(dynamodb.item(), next)
+    }, (err) => {
+      assert.ifError(err)
+      assert.equal(this.writer.queue.length, 0)
+      done(null)
+    })
+  })
+
+  it('should be able to manually call flush', function (done) {
+    async.times(10, (n, next) => {
+      this.writer.save(dynamodb.item(), next)
+    }, (err) => {
+      assert.ifError(err)
+      assert.equal(this.writer.queue.length, 10)
+      this.writer.flush((err) => {
+        assert.ifError(err)
+        assert.equal(this.writer.queue.length, 0)
+        done(null)
+      })
+    })
+  })
+
+  it('should be able to flush with empty queue', function (done) {
+    assert.equal(this.writer.queue.length, 0)
+    this.writer.flush(done)
+  })
+})
---
+++ test/util/dynamodb.js
@@ -0,0 +1,87 @@
+'use strict'
+
+const {DynamoDB} = require('aws-sdk')
+const Dynalite = require('dynalite')
+const DynamoDbWriter = require('../../src/')
+const debug = require('debug')('dynamodb-writer:test:util:dynamodb')
+
+const PORT = 4567
+const TABLE_NAME = 'dynamodb-writer-test'
+
+exports.createClient = function createClient () {
+  return new DynamoDB({
+    apiVersion: '2012-08-10',
+    region: 'us-east-1',
+    endpoint: 'http://localhost:' + PORT
+  })
+}
+
+exports.createWriter = function createWriter (dynamodb) {
+  return DynamoDbWriter.create(dynamodb, {table: TABLE_NAME})
+}
+
+exports.dynalite = function dynalite (cb) {
+  debug('Creating dynalite server on port ' + PORT)
+  return Dynalite({
+    createTableMs: 0,
+    deleteTableMs: 0
+  }).listen(PORT, cb)
+}
+
+exports.createTable = function createTable (dynamodb, cb) {
+  debug('creating table...')
+  dynamodb.createTable({
+    TableName: TABLE_NAME,
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5
+    },
+    AttributeDefinitions: [
+      {
+        AttributeName: 'id',
+        AttributeType: 'N'
+      },
+      {
+        AttributeName: 'created',
+        AttributeType: 'N'
+      }
+    ],
+    KeySchema: [
+      {
+        AttributeName: 'id',
+        KeyType: 'HASH'
+      },
+      {
+        AttributeName: 'created',
+        KeyType: 'RANGE'
+      }
+    ]
+  }, (err) => {
+    if (err) return cb(err)
+    cb(null)
+  })
+}
+
+exports.deleteTable = function deleteTable (dynamodb, cb) {
+  debug('Deleting table...')
+  dynamodb.deleteTable({
+    TableName: TABLE_NAME
+  }, (err) => {
+    if (err) return cb(err)
+    cb(null)
+  })
+}
+
+exports.item = function item (opts = {}) {
+  return {
+    id: {N: opts.id || id()},
+    created: {N: opts.created || String(Date.now())}
+  }
+}
+
+const id = (function () {
+  let lastId = 0
+  return function id () {
+    return String(++lastId)
+  }
+})()
